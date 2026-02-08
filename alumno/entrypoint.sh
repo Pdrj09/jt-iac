acea683954fb67f3393c1d1ad287223a195d1918
Pdrj09 <pdrj09@users.noreply.github.com>
2026-02-08 14:55:14 +0100

    add entrypoint.sh

---
+++ alumno/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "root:&{SSH_PASSWORD:-formacion}" | chpasswd
+
+dockered --host=unix://var/run/docker.sock &
+
+echo "Esperando a docker..."
+for i in $(seq 1 30); do
+    if docker info &>/dev/null; then
+        echo "ready ;)"
+        break
+    fi 
+        sleep 1
+done
+
+nginx
+
+echo /usr/sbin/sshd -D
+
